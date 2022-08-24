a4df65bf78cdb89148a35278e46643addd26d267
pradeepkumar <pradeepkumar@softence.com>
2022-08-24 12:36:18 +0530

    Reverted code

--- android/src/main/java/com/starlight/starlight_epub_viewer/Reader.java
+++ android/src/main/java/com/starlight/starlight_epub_viewer/Reader.java
@@ -12,7 +12,6 @@ import com.folioreader.model.HighLight;
 import com.folioreader.model.locators.ReadLocator;
 import com.folioreader.util.OnHighlightListener;
 import com.folioreader.util.ReadLocatorListener;
-import com.folioreader.ui.base.OnSaveHighlight;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -44,33 +43,31 @@ public class Reader implements OnHighlightListener, ReadLocatorListener, FolioRe
 
     public void open(String bookPath) {
         path = bookPath;
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    final Cursor _data = locatorDatabase.getReadableDatabase().rawQuery("SELECT * "
-                            + "FROM " + LocatorScheme.LocatorEntry.TABLE + " WHERE `" + LocatorScheme.LocatorEntry.ID
-                            + "` = ?", new String[] { path.split("/")[path.split("/").length - 1] });
-                    if (_data.moveToLast()) {
-                        ReadLocator readLocator = ReadLocator.fromJson(
-                                "{\"bookId\":\"" + _data.getString(_data.getColumnIndex("bookId")) + "\",\"href\":\""
-                                        + _data.getString(_data.getColumnIndex("href")) + "\",\"created\":\""
-                                        + _data.getString(_data.getColumnIndex("created")) + "\",\"locations\":{\"cfi\":\""
-                                        + _data.getString(_data.getColumnIndex("locations")) + "\"},\"title\":\"\"}");
+         new Thread(() -> {
+            try {
+                final Cursor _data = locatorDatabase.getReadableDatabase().rawQuery("SELECT * "
+                        + "FROM " + LocatorScheme.LocatorEntry.TABLE + " WHERE `" + LocatorScheme.LocatorEntry.ID
+                        + "` = ?", new String[] { path.split("/")[path.split("/").length - 1] });
+                if (_data.moveToLast()) {
+                    ReadLocator readLocator = ReadLocator.fromJson(
+                            "{\"bookId\":\"" + _data.getString(_data.getColumnIndex("bookId")) + "\",\"href\":\""
+                                    + _data.getString(_data.getColumnIndex("href")) + "\",\"created\":\""
+                                    + _data.getString(_data.getColumnIndex("created")) + "\",\"locations\":{\"cfi\":\""
+                                    + _data.getString(_data.getColumnIndex("locations")) + "\"},\"title\":\"\"}");
                         folioReader.setReadLocator(readLocator);
                     }
                     _data.close();
-                    folioReader
-                            .setConfig(readerConfig.config, true)
-                            .openBook(path);
-                } catch (Exception e) {
-                    Log.i("EPUB OPEN ERROR", e.toString());
-                    Log.i("EPUB OPEN ERROR", e.getLocalizedMessage());
-                    Log.i("EPUB OPEN ERROR", e.getMessage());
-                    Log.i("EPUB OPEN ERROR", Arrays.toString(e.getStackTrace()));
-                    e.printStackTrace();
-                }
-            }
+                folioReader
+                        .setConfig(readerConfig.config, true)
+                        .openBook(path);
+                // folioReader.openBook(path);
+
+            } catch (Exception e) {
+                Log.i("EPUB OPEN ERROR", e.toString());
+                Log.i("EPUB OPEN ERROR", e.getLocalizedMessage());
+                Log.i("EPUB OPEN ERROR", e.getMessage());
+                Log.i("EPUB OPEN ERROR", Arrays.toString(e.getStackTrace()));
+                e.printStackTrace();}
         }).start();
     }
 
@@ -80,33 +77,22 @@ public class Reader implements OnHighlightListener, ReadLocatorListener, FolioRe
     }
 
     private void getHighlightsAndSave() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                ArrayList<HighLight> highlightList = null;
-                ObjectMapper objectMapper = new ObjectMapper();
-                try {
-                    highlightList = objectMapper.readValue(
-                            loadAssetTextAsString(),
-                            new TypeReference<List<HighlightData>>() {
-                            });
-                } catch (IOException e) {
-                    Log.i("EPUB HIGHLIGHT ERROR", e.toString());
-                    Log.i("EPUB HIGHLIGHT ERROR", e.getLocalizedMessage());
-                    Log.i("EPUB HIGHLIGHT ERROR", e.getMessage());
-                    Log.i("EPUB HIGHLIGHT ERROR", Arrays.toString(e.getStackTrace()));
-                    Log.i("EPUB HIGHLIGHT ERROR", Arrays.toString(e.getStackTrace()));
-                    e.printStackTrace();
-                }
-
-                if (highlightList != null) {
-                    folioReader.saveReceivedHighLights(highlightList, new OnSaveHighlight() {
-                        @Override
-                        public void onFinished() {
-                            // You can do anything on successful saving highlight list
-                        }
-                    });
-                }
+         new Thread(() -> {
+            ArrayList<HighLight> highlightList = null;
+            ObjectMapper objectMapper = new ObjectMapper();
+            try {
+                highlightList = objectMapper.readValue(
+                        loadAssetTextAsString(),
+                        new TypeReference<List<HighlightData>>() {
+                        });
+            } catch (IOException e) {
+                Log.i("EPUB HIGHLIGHT ERROR", e.toString());
+                Log.i("EPUB HIGHLIGHT ERROR", e.getLocalizedMessage());
+                Log.i("EPUB HIGHLIGHT ERROR", e.getMessage());
+                Log.i("EPUB HIGHLIGHT ERROR", Arrays.toString(e.getStackTrace()));
+                Log.i("EPUB HIGHLIGHT ERROR", Arrays.toString(e.getStackTrace()));
+                e.printStackTrace();
+            }});        
             }
         }).start();
     }
